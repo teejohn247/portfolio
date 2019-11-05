2fe7212b384430417fa83b718e90e0650538084f
teejohn247 <teejohn247@gmail.com>
2019-11-05 17:30:10 +0100

    Added a readme file

---
+++ ReadMe.md
@@ -0,0 +1,29 @@
+PORTFOLIO
+
+This portfolio is a compilation of materials that exemplifies my beliefs, skills, qualifications, education, training, and experiences. It provides insight into my personality and work ethic
+
+------------------------------------------------------------------------------
+
+## This Portfolio was built with
+
+* HTML
+* CSS
+* BOOTSTRAP
+* Javascript
+
+### GitHub Pages link
+[AutoMart/UI link](https://teejohn247.github.io/portfolio)
+### SCREENSHOT
+![alt portfolio](https://teejohn247.github.io/portfolio/images/port.png)
+
+
+---------------------------------------------------------------------
+
+## Author
+- Tolu Ajuwon 
+- Email: teejohn247@gmail.com
+
+---
+
+## License & copyright
+Copyright (c) Tolu Ajuwon
--- index.js
+++ index.js
@@ -1,16 +1,49 @@
-const filter = () => {
-    let all =document.querySelector('.all');
-    let web =document.querySelector('.web');
-    let graphics =document.querySelector('.graphics');
+    let all =document.querySelector('.ali');
+    let web =document.querySelector('.webb');
+    let web1 =document.querySelectorAll('.website');
+    let graphbtn =document.querySelector('.graphic');
+
+    let graph =document.querySelectorAll('.graphics');
     let all1 = document.querySelectorAll('.all1');
     let hide = document.querySelector('.hide'); 
+    let now1 = document.querySelector('.now'); 
+    console.log(Array.from(web1))
+    let arr = Array.from(graph);
+    // arr.map(b => {
+    //     b.style.display = 'none'
+    // })
 
 
-    hide.style.display = 'none'
+//    $(document).ready(function(){
+//        $('.ali').click(function(){
+//            $('.graphics').show('fast')
+//            $('.website').show('fast')
+//        })
+//        $('.webb').click(function(){
+//         $('.graphics').hide()
+//     })
+//     $('.graphic').click(function(){
+//         $('.website').fadeOut('fast')
+//     })
+//    })
 
-   
 
 
-
-
-}
+     all.addEventListener('click', () => {
+         Array.from(web1).map(a => {
+             arr.map(b => b.style.display = "block")
+            a.style.display = 'block';
+         })
+     })
+     web.addEventListener('click', () => {
+         Array.from(web1).map(a => {
+             arr.map(b => b.style.display = "none")
+             a.style.display = 'block';
+         })
+     })
+     graphbtn.addEventListener('click', () => {
+         Array.from(graph).map(a => {
+             Array.from(web1).map(b => b.style.display = "none")
+             a.style.display = 'block';
+         })
+    })
